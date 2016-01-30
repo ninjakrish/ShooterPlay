852d9375b14ee815dbb1a907565a5a3f0dbfb737
Krish Soni <krishsoni6@gmail.com>
2016-01-30 15:39:50 -0500

    Added a rotating circle and changed speed of bounce

--- processing/game.js
+++ processing/game.js
@@ -4,8 +4,13 @@ var game = {
 
     objects: {
         player: {
-            radius: 20,
+            bounceAcceleration: 1.3,
 
+            spinSpeed: 0.003,
+            spinDirection: false,
+            theta: 0,
+
+            radius: 20,
             speed: {
                 x: 0.1,
                 y: -0.1
@@ -16,6 +21,7 @@ var game = {
     },
 
     engine: {
+        firstTick: 0,
         lastTick: 0,
         currentTick: 0,
         interval: 0,
@@ -41,6 +47,7 @@ var game = {
         processing.smooth();
         processing.ellipseMode(processing.CENTER);
 
+        game.engine.firstTick = game._now();
         game.engine.currentTick = game._now();
 
         game.engine.xMax = document.body.clientWidth;
@@ -52,35 +59,54 @@ var game = {
     },
 
     tick: function(processing) {
-        game.engine.lastTick = game.engine.currentTick;
-        game.engine.currentTick = game._now();
-        game.engine.interval = game.engine.currentTick - game.engine.lastTick;
+        if (game.engine.currentTick - game.engine.firstTick < 30000) {
+            game.engine.lastTick = game.engine.currentTick;
+            game.engine.currentTick = game._now();
+            game.engine.interval = game.engine.currentTick - game.engine.lastTick;
 
-        game.clearScreen(processing);
-        game.drawPlayer(processing, game.engine.counter, 400);
-
+            game.clearScreen(processing);
+            game.drawPlayer(processing, game.engine.counter, 400);
+        }
     },
 
     drawPlayer: function(processing) {
-        var deltaX = game.engine.interval * game.objects.player.speed.x;
-        var deltaY = game.engine.interval * game.objects.player.speed.y;
+        var player = game.objects.player;
 
-        var newX = game.objects.player.x + deltaX;
-        var newY = game.objects.player.y + deltaY;
-        var radius = game.objects.player.radius;
+        var deltaX = game.engine.interval * player.speed.x;
+        var deltaY = game.engine.interval * player.speed.y;
+
+        var newX = player.x + deltaX;
+        var newY = player.y + deltaY;
+        var radius = player.radius;
 
         if (newY - radius < game.engine.yMin || newY + radius > game.engine.yMax) {
-            game.objects.player.speed.y = game.objects.player.speed.y * -1;
+            player.speed.y = player.speed.y * -1.0 * player.bounceAcceleration;
+            player.speed.x = player.speed.x * player.bounceAcceleration;
+            player.spinSpeed = player.spinSpeed * player.bounceAcceleration;
+            player.spinDirection = !player.spinDirection;
         }
         if (newX - radius < game.engine.xMin || newX + radius > game.engine.xMax) {
-            game.objects.player.speed.x = game.objects.player.speed.x * -1;
+            player.speed.x = player.speed.x * -1.0 * player.bounceAcceleration;
+            player.speed.y = player.speed.y * player.bounceAcceleration;
+            player.spinSpeed = player.spinSpeed * player.bounceAcceleration;
+            player.spinDirection = !player.spinDirection;
         }
-        game.objects.player.x = newX;
-        game.objects.player.y = newY;
+
+        player.x = newX;
+        player.y = newY;
 
         processing.stroke(255, 30, 0);
         processing.fill(100, 80, 70);
-        processing.ellipse(game.objects.player.x, game.objects.player.y, game.objects.player.radius * 2, game.objects.player.radius * 2);
+
+        processing.ellipse(player.x, player.y, radius * 2, radius * 2);
+
+        processing.stroke(30, 255, 0);
+        processing.fill(20, 30, 150);
+
+        player.theta = player.theta + (player.spinDirection ? 1.0 : -1.0) * player.spinSpeed * game.engine.interval;
+        processing.ellipse(player.x + Math.cos(player.theta) * radius * 2,
+                           player.y + Math.sin(player.theta) * radius * 2,
+                           radius * 0.2, radius * 0.2);
     },
 
     clearScreen: function(processing) {
