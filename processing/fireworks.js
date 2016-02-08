2ea4fa4d10e2d6d951ac8a57df96022cc62a3593
Hiren <hhiranan@gmail.com>
2016-02-07 19:38:30 -0500

    fireworks setup.

--- processing/fireworks.js
+++ processing/fireworks.js
@@ -1,18 +1,52 @@
 function fireworks() {
-    return [
-        {
-            startingTime: 1.0,
-            color: {
-                r: 100,
-                g: 100,
-                b: 100
-            },
-            timer: 3.0,
-            onExplode: function(factory) {
-                factory.addFirework({
-
-                });
-            }
-        }
+    var all = [
+        first
     ];
+
+    var first = {
+        startingTime: 1.0,
+        angle: 90,
+        speed: 10,
+        color: {
+            r: 100,
+            g: 100,
+            b: 100
+        },
+        timer: 3.0,
+        onInit: function(factory) {
+            first.variable = true;
+        },
+        onTick: function(factory) {
+            console.log("variable: " + first.variable);
+            if (first.color.r > 0) {
+                first.color.r = first.color.r - 1;
+            }
+            if (first.color.g > 0) {
+                first.color.g = first.color.g - 1;
+            }
+            if (first.color.b > 0) {
+                first.color.b = first.color.b - 1;
+            }
+        },
+        onExplode: function(factory) {
+            factory.projectile([
+                {
+                    angles: 12,
+                    speed: 1.0,
+                    color: {
+                        r: 100,
+                        g: 0,
+                        b: 0
+                    },
+                    onTick: function(time change) {
+                        if (first.color.r > 0) {
+                            first.color.r = first.color.r - 1;
+                        }
+                    }
+                }
+            ]);
+        }
+    };
+
+    return all;
 }
